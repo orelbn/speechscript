88763b691c2a23455ec676c61e241b7791144d95
Weike Qu <weikequ@hotmail.com>
2022-01-15 23:40:21 -0800

    fix: removed container padding

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -3,7 +3,7 @@ import Container from '@mui/material/Container';
 
 export default function IntroPage(props) {
     return (
-        <Container sx={{height: "100vh", scrollSnapAlign: "center", backgroundColor: "#e1e1e1"}}>
+        <Container maxWidth={false} disableGutters={true} sx={{height: "100vh", scrollSnapAlign: "center", backgroundColor: "#e1e1e1"}}>
         </Container>
     )
 }
