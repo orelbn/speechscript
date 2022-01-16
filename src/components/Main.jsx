b4860b4d0337dfa7801bfbf8d13ee8afc2bfa9f2
limseung1 <you@example.com>
2022-01-15 17:36:52 -0800

    feat: fetch openAI API

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { React, useState } from 'react'
+import { getCode } from '../script/fetchopenai'
 
-const Main = () => {
+export const Main = () => {
+    const [codeData, setcodeData] = useState("")
+
+    /**
+     * fetches result from openAI API based on user's command, when user clicks Run
+     */
+    const handleOpenAIfetch = async() => {
+        const command = "Add a button and center into the middle of the page"
+        const codes =  await getCode(command)
+        setcodeData(codes.choices[0].text)
+        console.log(codes.choices[0].text)
+    }
     return (
         <div>
-            
+            <button onClick={handleOpenAIfetch}>Run</button>
         </div>
     )
 }
