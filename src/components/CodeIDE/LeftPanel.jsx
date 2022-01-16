a5d92839de3d60cf98412d863b1fdd12ca242b29
alex-clements <ahclemen@ualberta.ca>
2022-01-16 10:34:42 -0800

    fixed the execute button

--- src/components/CodeIDE/LeftPanel.jsx
+++ src/components/CodeIDE/LeftPanel.jsx
@@ -29,8 +29,20 @@ export default function LeftPanel(props) {
             <Box sx={{display: 'flex', justifyContent: "center", marginTop: 2}}>
                 <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
                     <Box sx={{display: 'flex', flexDirection: 'column', height: "15vh", marginRight: 5, width:170}}>
-                        <Button  variant="contained" onClick={handleOpenAIfetch} sx={{height: 50}}>Execute</Button>
-                        <Button className={recording && "recording"} color="error" variant="contained" onClick={handleRecordSpeech} sx={{height: 50, marginTop: 3}}>Record</Button>
+                        <Button  
+                        variant="contained" 
+                        onClick={handleOpenAIfetch} 
+                        sx={{height: 50, backgroundColor: "#ffc5c5", "&:hover": {backgroundColor: "#e8bcbc"}}}
+                        >
+                        Execute
+                        </Button>
+                        <Button className={recording && "recording"}
+                                color="error" 
+                                variant="contained" 
+                                onClick={handleRecordSpeech} 
+                                sx={{height: 50, marginTop: 3}}>
+                        Record
+                        </Button>
                     </Box>
                     <Box sx={{display: 'flex', flexDirection: 'column', marginX: 5}}>
                         <input style={{height:"100%", width:"133%", backgroundColor: "black", borderRadius: "10px", color: "white", borderWidth: "0px"}} type="text" onChange={(e) => { 
